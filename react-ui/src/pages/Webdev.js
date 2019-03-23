c2cb9d8420a4d69d3da4ef013812442e202f35d0
Carlos Michael Rodriguez <cmr624@nyu.edu>
2019-03-23 11:20:36 -0400

    #13 done

---
+++ react-ui/src/pages/Webdev.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import logo from '.././logo.svg';
+class Webdev extends Component {
+
+  render() {
+    return (
+        <h1>WEBDEV</h1>
+    );
+  }
+}
+
+export default Webdev;
